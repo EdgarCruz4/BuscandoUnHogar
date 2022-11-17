dda4e59e93681a73278640c53f39935575ecf30f
Alain Eustaquio Rivera Rios <aln.r.r.91@icloud.com>
2022-11-17 13:21:50 -0600

    Avance_11172022

---
+++ nuevo.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Buscando Un Hogar</title>
+    <link rel="stylesheet" type="text/css" href="css/nuevo.css">
+    <link rel="stylesheet" href="css/navbar.css">
+</head>
+<body>
+<header class="navbar">
+<div class="logo">
+<div class="gato"></div>
+<div class="perro"></div>
+<h1>Buscando Un Hogar</h1>
+</div>
+<nav class="menu">
+<ul>
+<li>
+<a href="#">
+Donaciones <img class="flecha" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAOBJREFUSEvtlN0NwjAMhH2b0E3oJjAJMAlsQjehmxwyElHS5sdJyBt+q+r44u/sQAYHBteXv0CRsENE8i4ip+IJW8IDwFlTfYGXiBxs54tZK4BpK6DFnz8SmQEsgYB+kLyKyKV4v3zCDYDW+cRuTEn2oHJocgI9qCYAq99gdNEaUQVokh18f5BUw49GPxYAcyw3+VSQtKLacS8i8rqwTJUbyaoOjCJuY1MoTa9pYnSzaIom+zciqWar6X5k0VQJRLY8OpJNHmw60S1XNNGR7BYw7kSQZjK5pXC1B60iwzt4A/3qSxkbGxzrAAAAAElFTkSuQmCC" />
+</a>
+<ul>
+<li><a href="CatalogoGato.php">Gato</a></li>
+<li><a href="CatalogoPerro.php">Perro</a></li>
+</ul>
+</li>
+<li>
+<a href="#">
+Adoptar <img class="flecha" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAOBJREFUSEvtlN0NwjAMhH2b0E3oJjAJMAlsQjehmxwyElHS5sdJyBt+q+r44u/sQAYHBteXv0CRsENE8i4ip+IJW8IDwFlTfYGXiBxs54tZK4BpK6DFnz8SmQEsgYB+kLyKyKV4v3zCDYDW+cRuTEn2oHJocgI9qCYAq99gdNEaUQVokh18f5BUw49GPxYAcyw3+VSQtKLacS8i8rqwTJUbyaoOjCJuY1MoTa9pYnSzaIom+zciqWar6X5k0VQJRLY8OpJNHmw60S1XNNGR7BYw7kSQZjK5pXC1B60iwzt4A/3qSxkbGxzrAAAAAElFTkSuQmCC" />
+</a>
+<ul>
+<li><a href="CatalogoGato.php">Gato</a></li>
+<li><a href="CatalogoPerro.php">Perro</a></li>
+</ul>
+</li>
+</ul>
+</nav>
+</header>
+<div class="modal">
+<div class="content-left">
+
+</div>
+<div class="content-right">
+<div class="content-header">
+<div class="content">
+<p id="modal-title">Inicio de sesion</p>
+<div class="footprint">
+<div class="user-icon"></div>
+</div>
+</div>
+</div>
+<div class="content-body">
+<form action="">
+<div class="form-content">
+<div>
+<div class="input-user"><label for="username">Usuario:</label></div>
+<br>
+<div class="input-pass"><label for="password">Contraseña:</label></div>
+</div>
+<div>
+<input name="username" type="text" placeholder="Juan">
+<br>
+<input name="password" type="password" placeholder="12345">
+</div>
+</div>
+<div class="btn">
+<button type="submit" id="btn-submit">Entrar</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+</body>
+</html>
